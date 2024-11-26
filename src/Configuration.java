12bd4e47c68ccd1dafcabc989f73f1d9a5c05f2b
Shavinkar-SKR <shavinkarskr17@gmail.com>
2024-11-26 16:55:38 +0530

    Adding files

---
+++ src/Configuration.java
@@ -0,0 +1,5 @@
+public class Configuration {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Vendor.java
@@ -0,0 +1,9 @@
+public class Vendor {
+    private int vendorID;
+    private int ticketsPerRelease;
+    private int releaseInterval;
+
+    public Vendor(int vendorID, int ticketsPerRelease, int releaseInterval){
+
+    }
+}
