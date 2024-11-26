9ae5920bc494c11a237729d5a6862f370e0e5948
Shavinkar-SKR <shavinkarskr17@gmail.com>
2024-11-27 00:36:18 +0530

    Updating Vendor file

--- src/model/ticketingSystem/coursework/oop/Vendor.java
+++ src/model/ticketingSystem/coursework/oop/Vendor.java
@@ -12,5 +12,32 @@ public class Vendor {
         this.releaseInterval = releaseInterval;
     }
 
+    public int getVendorID(){
+        return this.vendorID;
+    }
+
+    public void setVendorID(int vendorID){
+        this.vendorID = vendorID;
+    }
+
+    public int getTicketsPerRelease(){
+        return this.ticketsPerRelease;
+    }
+
+    public void setTicketsPerRelease(int ticketsPerRelease){
+        this.ticketsPerRelease = ticketsPerRelease;
+    }
+
+    public int getReleaseInterval(){
+        return this.releaseInterval;
+    }
+
+    public void setReleaseInterval(int releaseInterval){
+        this.releaseInterval = releaseInterval;
+    }
+
+    public void run(){
+
+    }
 
 }
